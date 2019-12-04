4ca2de278fa9057a746beaeba7704d2fe6455c76
BoKyung <bo216@naver.com>
2019-12-04 17:19:51 +0900

    DFS,재귀

---
+++ src/test/programmers/TheLargestNumber.java
@@ -0,0 +1,27 @@
+package test.programmers;
+import java.util.*;
+
+public class TheLargestNumber {
+	
+	static public String solution(int[] numbers) {
+        String answer = "";
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=0;i<numbers.length;i++) {
+        	
+        }
+        
+        for(int i=0; i<numbers.length; i++) {
+        	answer+=numbers[i];
+        	
+        }
+        
+        return answer;
+    }
+	public static void main(String[] args) {
+		int[] numbers = {6,10,2};;
+		System.out.println(solution(numbers));
+		
+	}
+
+}
