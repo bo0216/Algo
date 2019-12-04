fe53d4c3eeed0bfd9dbca2cbc65d6bc2f6965774
BoKyung <bo216@naver.com>
2019-12-04 17:24:10 +0900

    DFS,재귀

---
+++ src/test/programmers/TargetNumber.java
@@ -0,0 +1,29 @@
+package test.programmers;
+import java.util.*;
+
+public class TargetNumber {
+	static public int solution(int[] numbers, int target) {
+        int answer = 0;
+        answer = dfs(numbers, 0, 0, target);
+        return answer;
+    }
+	
+	static public int dfs(int[] numbers, int node, int sum, int target) {
+		if(node == numbers.length) {
+			if(sum == target) 
+				return 1;
+			return 0;
+		}//
+		return dfs(numbers, node+1, sum+numbers[node], target)
+				+ dfs(numbers, node+1, sum-numbers[node], target);
+	}
+	
+	public static void main(String[] args) {
+		int[] numbers = {2,3,1};
+		int target = 5;
+		
+		int s = solution(numbers,target);
+		System.out.println(s);
+	}
+
+}
