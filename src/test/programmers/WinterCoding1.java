9ef9feccfb743e63ce9457914afcce5cfe3a5006
bo0216 <bo216@naver.com>
2019-12-17 18:45:05 +0900

    프로그래머스-멀쩡한 사각형/ 대각선-> 최대공약수

---
+++ src/test/programmers/WinterCoding1.java
@@ -0,0 +1,31 @@
+package test.programmers;
+import java.util.*;
+
+public class WinterCoding1 { 
+	static public long solution(long w,long h) {
+		long answer = 1;
+		
+		if(w==1 || h==1) {
+			return 0;
+		}else {
+			long g = GCD(w,h);
+			answer = w*h - (w+h-g);
+			
+			return answer;
+		}
+	}
+	
+	static public long GCD(long a,long b) { //최대공약수 구하기 
+		if(b==0)
+			return a;
+		return GCD(b,a%b);
+	}
+	
+	public static void main(String[] args) {
+		int w = 8;
+		int h = 12;
+		
+		System.out.println(solution(w,h));
+	}
+
+}
